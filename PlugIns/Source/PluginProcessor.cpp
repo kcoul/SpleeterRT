6273df71ec724542a94a25826b13950917f2ee95
kcoul <kieran.coulter@gmail.com>
2023-04-21 18:12:58 -0700

    Add CMake for plugin, organize folder, fix home folder bug on unix

--- PlugIns/Source/PluginProcessor.cpp
+++ PlugIns/Source/PluginProcessor.cpp
@@ -30,11 +30,17 @@ const float paramValueRange[PARAM__MAX][5] =
         return TRUE;
     }
 #else
-bool getCurrentUserDirUnix(char* buf, int buflen)
-{
-    buf = getenv("HOME");
-    return true;
-}
+    #include <unistd.h>
+    #include <sys/types.h>
+    #include <pwd.h>
+    bool getCurrentUserDirUnix(char* buf, int buflen)
+    {
+        if (getenv("HOME") == NULL)
+            strcpy(buf, getpwuid(getuid())->pw_dir);
+        else
+            strcpy(buf, getenv("HOME"));
+        return true;
+    }
 #endif
 class MS5_1AI : public AudioProcessor, public AudioProcessorValueTreeState::Listener
 {
@@ -59,10 +65,17 @@ public:
 		for (i = 0; i < 4; i++)
 			coeffProvPtr[i] = malloc(getCoeffSize());
 		// Load coeff
+#ifdef _WIN32
 		char file1[17] = "\\drum4stems.dat";
 		char file2[17] = "\\bass4stems.dat";
 		char file3[26] = "\\accompaniment4stems.dat";
 		char file4[17] = "\\vocal4stems.dat";
+#else
+        char file1[17] = "/drum4stems.dat";
+        char file2[17] = "/bass4stems.dat";
+        char file3[26] = "/accompaniment4stems.dat";
+        char file4[17] = "/vocal4stems.dat";
+#endif
 		size_t len1 = strlen(dir);
 		size_t len2 = strlen(file1);
 		char *concat = (char*)malloc(len1 + len2 + 1);
@@ -151,53 +164,50 @@ public:
 		// input channels
 		const float *inputs[2] = { buffer.getReadPointer(0), buffer.getReadPointer(1) };
 		float *tmpPtr1, *tmpPtr2;
-		float* outputs[8] = { buffer.getWritePointer(0), buffer.getWritePointer(1),
-		buffer.getWritePointer(2), buffer.getWritePointer(3), buffer.getWritePointer(4),
-		buffer.getWritePointer(5), buffer.getWritePointer(6), buffer.getWritePointer(7) };
-		if (order == 1)
-		{
-			tmpPtr1 = outputs[0];
-			tmpPtr2 = outputs[1];
-			outputs[0] = outputs[2];
-			outputs[1] = outputs[3];
-			outputs[2] = tmpPtr1;
-			outputs[3] = tmpPtr2;
-		}
-		else if (order == 2)
-		{
-			tmpPtr1 = outputs[0];
-			tmpPtr2 = outputs[1];
-			outputs[0] = outputs[4];
-			outputs[1] = outputs[5];
-			outputs[4] = tmpPtr1;
-			outputs[5] = tmpPtr2;
-		}
-		else if (order == 3)
-		{
-			tmpPtr1 = outputs[0];
-			tmpPtr2 = outputs[1];
-			outputs[0] = outputs[6];
-			outputs[1] = outputs[7];
-			outputs[6] = tmpPtr1;
-			outputs[7] = tmpPtr2;
-		}
-		// output channels
-		int i, offset = 0;
-		while (offset < n)
-		{
-			float *ptr[8] = { outputs[0] + offset, outputs[1] + offset,
-			outputs[2] + offset, outputs[3] + offset, outputs[4] + offset,
-			outputs[5] + offset, outputs[6] + offset, outputs[7] + offset };
-			const int processing = min(n - offset, OVPSIZE);
-			Spleeter4StemsProcessSamples(msr, inputs[0] + offset, inputs[1] + offset, processing, ptr);
-			offset += processing;
-		}
+
+        if (buffer.getNumChannels() == 8) {
+            float *outputs[8] = {buffer.getWritePointer(0), buffer.getWritePointer(1),
+                                 buffer.getWritePointer(2), buffer.getWritePointer(3), buffer.getWritePointer(4),
+                                 buffer.getWritePointer(5), buffer.getWritePointer(6), buffer.getWritePointer(7)};
+            if (order == 1) {
+                tmpPtr1 = outputs[0];
+                tmpPtr2 = outputs[1];
+                outputs[0] = outputs[2];
+                outputs[1] = outputs[3];
+                outputs[2] = tmpPtr1;
+                outputs[3] = tmpPtr2;
+            } else if (order == 2) {
+                tmpPtr1 = outputs[0];
+                tmpPtr2 = outputs[1];
+                outputs[0] = outputs[4];
+                outputs[1] = outputs[5];
+                outputs[4] = tmpPtr1;
+                outputs[5] = tmpPtr2;
+            } else if (order == 3) {
+                tmpPtr1 = outputs[0];
+                tmpPtr2 = outputs[1];
+                outputs[0] = outputs[6];
+                outputs[1] = outputs[7];
+                outputs[6] = tmpPtr1;
+                outputs[7] = tmpPtr2;
+            }
+            // output channels
+            int i, offset = 0;
+            while (offset < n) {
+                float *ptr[8] = {outputs[0] + offset, outputs[1] + offset,
+                                 outputs[2] + offset, outputs[3] + offset, outputs[4] + offset,
+                                 outputs[5] + offset, outputs[6] + offset, outputs[7] + offset};
+                const int processing = min(n - offset, OVPSIZE);
+                Spleeter4StemsProcessSamples(msr, inputs[0] + offset, inputs[1] + offset, processing, ptr);
+                offset += processing;
+            }
+        }
 	}
 
 	//==============================================================================
 	AudioProcessorEditor* createEditor() override { return new GenericAudioProcessorEditor(*this); }
 	bool hasEditor() const override { return true; }
-	const String getName() const override { return "Spleeter4Stems"; }
+	const String getName() const override { return "SpleeterRTPlug"; }
 	bool acceptsMidi() const override { return false; }
 	bool producesMidi() const override { return false; }
 	double getTailLengthSeconds() const override { return 0.0; }
